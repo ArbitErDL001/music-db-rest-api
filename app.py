607d7de3e2f14b1d96ccbe6210227f6e87968352
ArbitErDL001 <202380330@psu.palawan.edu.ph>
2025-12-15 22:47:43 +0800

    database connection

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, make_response
+import mysql.connector
+import xml.etree.ElementTree as ET
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+# ---------------- DATABASE CONNECTION ----------------
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
